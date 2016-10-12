4b8a8c7384f43a7bac339bf3b098859d0669f0e4
liuyuesong <liuyuesong@kingsoft.com>
2016-10-12 14:03:43 +0800

    修复播放页的显示不一致

--- src/pages/local/play.js
+++ src/pages/local/play.js
@@ -8,24 +8,29 @@ export default class Play extends wx.Component{
   	item: {},
   	isPlaying: false
   }
-	async onLoad(option){
-		if(option.songid){
-			const data = await util.playSong(option.songid)
-			this.setData({
-				isPlaying: true,
-				data
-			})
-			await wx.setStorage({key: 'playing', data: data})
-		} else {
-			const res = await wx.getStorage({key: 'playing'})
-			this.setData(res.data)
-		}
+  async updateSong(songid){
+  	if(songid){
+  		const data = await util.playSong(songid)
+  		this.setData({
+  			isPlaying: true,
+  			...data
+  		})
+  		await wx.setStorage({key: 'playing', data: data})
+  	} else {
+  		const res = await wx.getStorage({key: 'playing'})
+  		this.setData(res.data)
+  	}
 
-		const res = await wx.getStorage({key: 'isPlaying'})
-		this.setData({
-			isPlaying: res.data
-		})
-
+  	const res = await wx.getStorage({key: 'isPlaying'})
+  	this.setData({
+  		isPlaying: res.data
+  	})
+  }
+	onLoad(option){
+		this.updateSong(option.songid)
+	}
+	onShow(){
+		this.updateSong()
 	}
 	async playToggle(e){
 		if(this.data.isPlaying){
